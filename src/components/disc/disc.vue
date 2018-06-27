de7fba7e69d2fbe4f09301d59dc484a90d8cab5c
heroMRQ <463363460@qq.com>
2018-06-27 23:47:28 +0800

    联系开发环境完成

---
+++ src/components/disc/disc.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition name="slide">
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+  </transition>
+</template>
+
+<script type="text/ecmascript-6">
+  // import MusicList from 'components/music-list/music-list'
+  // import {getSongList} from 'api/recommend'
+  // import {ERR_OK} from 'api/config'
+  // import {mapGetters} from 'vuex'
+  // import {createSong} from 'common/js/song'
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+  .slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="singer" ref="singer">
+    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  // import ListView from 'base/listview/listview'
+  // import {getSingerList} from 'api/singer'
+  // import {ERR_OK} from 'api/config'
+  // import Singer from 'common/js/singer'
+  // import {mapMutations} from 'vuex'
+  // import {playlistMixin} from 'common/js/mixin'
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
