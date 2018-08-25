b3ff307c8a89ec7d05c8b93c6202c2d59a2afcb5
heroMRQ <463363460@qq.com>
2018-08-25 10:59:59 +0800

    增加domjs

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="rank" ref="rank">
-    <scroll :data="topList" class="toplist" ref="toplist">
-      <ul>
-        <li @click="selectItem(item)" class="item" v-for="item in topList">
-          <div class="icon">
-            <img width="100" height="100" v-lazy="item.picUrl"/>
-          </div>
-          <ul class="songlist">
-            <li class="song" v-for="(song,index) in item.songList">
-              <span>{{index + 1}}</span>
-              <span>{{song.songname}}-{{song.singername}}</span>
-            </li>
-          </ul>
+    <<ul>
+    <li @click="" class="item" >
+      <div class="icon">
+        <img width="100" height="100" />
+      </div>
+      <ul class="songlist">
+        <li class="song" >
+          <span></span>
+          <span></span>
         </li>
       </ul>
-      <div class="loading-container" v-show="!topList.length">
-        <loading></loading>
-      </div>
-    </scroll>
-    <router-view></router-view>
+    </li>
+  </ul>
+    <div class="loading-container" v-show="">
+      <!--<loading></loading>-->
+    </div>
+    <!--<router-view></router-view>-->
   </div>
 </template>
 
