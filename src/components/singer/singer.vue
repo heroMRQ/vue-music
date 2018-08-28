d0b2afb52caf7a114f250cab1366f14f1ee3ba73
heroMRQ <463363460@qq.com>
2018-08-28 23:53:42 +0800

    处理数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="singer" ref="singer">
-    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
-    <router-view></router-view>
+  <div class="singer">
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
-  // import ListView from 'base/listview/listview'
-  // import {getSingerList} from 'api/singer'
-  // import {ERR_OK} from 'api/config'
-  // import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
   // import {mapMutations} from 'vuex'
   // import {playlistMixin} from 'common/js/mixin'
 
+  const HOT_NAME = '热'
+  const HOT_SINGER_LEN = 10
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this._normalizeSinger(this.singers))
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          title: HOT_NAME,
+          items: []
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        console.log(map)
+        // let ret = []
+        // let hot = []
+        for (let key in map) {
+          // if (key.match(/[a-zA-Z]/)) {
+          console.log(key)
+          console.log(map[key])
+          // }
+        }
+        console.log(map)
+      }
+    },
+    components: {
+      ListView
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
