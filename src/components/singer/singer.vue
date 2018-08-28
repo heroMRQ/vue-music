28a1021cecbf15db683d9c9ac16c9c1b9358c109
maruiqian <maruiqian@btzh.cn>
2018-08-28 18:45:20 +0800

    开始开发歌手页面V1.1

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="singer" ref="singer">
-    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
-    <router-view></router-view>
   </div>
 </template>
 
 <script>
   // import ListView from 'base/listview/listview'
-  // import {getSingerList} from 'api/singer'
-  // import {ERR_OK} from 'api/config'
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
   // import Singer from 'common/js/singer'
   // import {mapMutations} from 'vuex'
   // import {playlistMixin} from 'common/js/mixin'
-
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then(res => {
+          if (res.code === ERR_OK) {
+            this.singer = res.data.list
+            console.log(this.singer)
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
