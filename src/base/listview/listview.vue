b051729724bf0fd74317626735f449366f85a493
maruiqian <maruiqian@btzh.cn>
2018-09-02 17:56:11 +0800

    开始开发歌手页面问题

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,46 +1,43 @@
 <template>
-  <scroll @scroll="scroll"
-          :listen-scroll="listenScroll"
-          :probe-type="probeType"
-          :data="data"
+  <Scroll :data="data"
           class="listview"
-          ref="listview">
+          ref="listview"
+          :probeType="3"
+          :listenScroll="listenScroll"
+          @scroll="scroll">
     <ul>
-      <li v-for="group in data" class="list-group" ref="listGroup">
+      <li v-for="group in data" class="list-group">
         <h2 class="list-group-title">{{group.title}}</h2>
-        <uL>
-          <li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
-            <img class="avatar" v-lazy="item.avatar">
+        <ul>
+          <li v-for="item in group.items" ref="listGroup" class="list-group-item">
+            <img :src="item.avatar" class="avatar">
             <span class="name">{{item.name}}</span>
           </li>
-        </uL>
+        </ul>
       </li>
     </ul>
-    <div class="list-shortcut" @touchstart.stop.prevent="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove"
-         @touchend.stop>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
       <ul>
-        <li v-for="(item, index) in shortcutList" :data-index="index" class="item"
-            :class="{'current':currentIndex===index}">{{item}}
+        <li v-for="(item,index) in shortcutList"
+            class="item"
+            :class="{'current': currentIndex === index}"
+            :data-index="index">
+          {{item}}
         </li>
       </ul>
     </div>
-    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
-      <div class="fixed-title">{{fixedTitle}} </div>
+    <div class="list-fixed" v-show="fixedTitle">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
     </div>
-    <div v-show="!data.length" class="loading-container">
-      <loading></loading>
-    </div>
-  </scroll>
+  </Scroll>
 </template>
 
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
-  import Loading from 'base/loading/loading'
+  //  import Loading from 'base/loading/loading'
   import {getData} from 'common/js/dom'
 
-  const TITLE_HEIGHT = 30
-  const ANCHOR_HEIGHT = 18
-
+  const ANVHOR_HEIGHT = 18
   export default {
     props: {
       data: {
@@ -48,68 +45,68 @@
         default: []
       }
     },
-    computed: {
-      shortcutList() {
-        return this.data.map((group) => {
-          return group.title.substr(0, 1)
-        })
+    created() {
+      this.touch = {}
+      this.listenScroll = true
+      this.listHeight = []
+      this.probeType = 3
+    },
+    data() {
+      return {
+        scrollY: -1,
+        currentIndex: 0
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this._calculateHeight()
+        }, 20)
+      },
+      scrollY(newY) {
+        const listHeight = this.listHeight
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        for (let i = 0; i < listHeight.length - 1; i++) {
+          let height1 = listHeight[i]
+          let height2 = listHeight[i + 1]
+          if (-newY >= height1 && -newY < height2) {
+            this.currentIndex = i
+            return
+          }
+        }
+        this.currentIndex = listHeight.length - 2
       },
       fixedTitle() {
         if (this.scrollY > 0) {
           return ''
         }
-        return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+        return this.data[this.currentIndex] ? this.data[this.currentIndex] : ''
       }
     },
-    data() {
-      return {
-        scrollY: -1,
-        currentIndex: 0,
-        diff: -1
+    computed: {
+      shortcutList() {
+        return this.data.map((group) => {
+          return group.title.substr(0, 1)
+        })
       }
-    },
-    created() {
-      this.probeType = 3
-      this.listenScroll = true
-      this.touch = {}
-      this.listHeight = []
     },
     methods: {
-      selectItem(item) {
-        this.$emit('select', item)
-      },
       onShortcutTouchStart(e) {
         let anchorIndex = getData(e.target, 'index')
         let firstTouch = e.touches[0]
-        this.touch.y1 = firstTouch.pageY
         this.touch.anchorIndex = anchorIndex
-
+        this.touch.y1 = firstTouch.pageY
         this._scrollTo(anchorIndex)
       },
       onShortcutTouchMove(e) {
         let firstTouch = e.touches[0]
         this.touch.y2 = firstTouch.pageY
-        let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
-        let anchorIndex = parseInt(this.touch.anchorIndex) + delta
-
+        let delta = (this.touch.y2 - this.touch.y1) / ANVHOR_HEIGHT | 0
+        let anchorIndex = parseInt(this.touch.anchorIndex + delta)
         this._scrollTo(anchorIndex)
-      },
-      refresh() {
-        this.$refs.listview.refresh()
-      },
-      scroll(pos) {
-        this.scrollY = pos.y
-      },
-      _calculateHeight() {
-        this.listHeight = []
-        const list = this.$refs.listGroup
-        let height = 0
-        this.listHeight.push(height)
-        for (let i = 0; i < list.length; i++) {
-          let item = list[i]
-          height += item.clientHeight
-          this.listHeight.push(height)
-        }
       },
       _scrollTo(index) {
         if (!index && index !== 0) {
@@ -122,46 +119,24 @@
         }
         this.scrollY = -this.listHeight[index]
         this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
-      }
-    },
-    watch: {
-      data() {
-        setTimeout(() => {
-          this._calculateHeight()
-        }, 20)
       },
-      scrollY(newY) {
-        const listHeight = this.listHeight
-        // 当滚动到顶部，newY>0
-        if (newY > 0) {
-          this.currentIndex = 0
-          return
-        }
-        // 在中间部分滚动
-        for (let i = 0; i < listHeight.length - 1; i++) {
-          let height1 = listHeight[i]
-          let height2 = listHeight[i + 1]
-          if (-newY >= height1 && -newY < height2) {
-            this.currentIndex = i
-            this.diff = height2 + newY
-            return
-          }
-        }
-        // 当滚动到底部，且-newY大于最后一个元素的上限
-        this.currentIndex = listHeight.length - 2
+      scroll(pos) {
+        this.scrollY = pos.y
+        console.log(this.scrollY)
       },
-      diff(newVal) {
-        let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
-        if (this.fixedTop === fixedTop) {
-          return
+      _calculateHeight() {
+        this.listHeight = []
+        const list = this.$refs.listGroup
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < list.length; i++) {
+          height += list[i].clientHeight
+          this.listHeight.push(height)
         }
-        this.fixedTop = fixedTop
-        this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
       }
     },
     components: {
-      Scroll,
-      Loading
+      Scroll
     }
   }
 
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="singer" ref="singer">
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
-  // import ListView from 'base/listview/listview'
+  import ListView from 'base/listview/listview'
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
-  // import Singer from 'common/js/singer'
+  import Singer from 'common/js/singer'
   // import {mapMutations} from 'vuex'
   // import {playlistMixin} from 'common/js/mixin'
+  const HOT_WORD = '热'
+  const HOT_SINGER_LENGTH = 10
   export default {
     data() {
       return {
@@ -23,11 +26,51 @@
       _getSingerList() {
         getSingerList().then(res => {
           if (res.code === ERR_OK) {
-            this.singer = res.data.list
-            console.log(this.singer)
+            this.singers = this._dealSingerData(res.data.list)
           }
         })
+      },
+      _dealSingerData(data) {
+        let map = {
+          hot: {
+            title: HOT_WORD,
+            items: []
+          }
+        }
+        data.forEach((item, index) => {
+          if (index < HOT_SINGER_LENGTH) {
+            map.hot.items.push(new Singer({id: item.Fsinger_mid, name: item.Fsinger_name}))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_WORD) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
